8f8a7d9e54e6bcdf07915cfbd177074502b97996
abcdw654 <ahmed.mohamed547@gmail.com>
2025-05-07 19:50:41 +0100

    fix: status not loading in settings page.

--- src/esphomepm/usr/local/emhttp/plugins/esphomepm/status.php
+++ src/esphomepm/usr/local/emhttp/plugins/esphomepm/status.php
@@ -5,10 +5,17 @@ $esphomepm_costs_price = isset($esphomepm_cfg['COSTS_PRICE']) ? $esphomepm_cfg['
 $esphomepm_costs_unit = isset($esphomepm_cfg['COSTS_UNIT']) ? $esphomepm_cfg['COSTS_UNIT'] : "GBP";
 
 if ($esphomepm_device_ip == "") {
-	die("ESPHome Device IP missing!");
+	header('Content-Type: application/json');
+	echo json_encode(['error' => 'ESPHome Device IP missing']);
+	exit;
 }
 
 $Url = "http://" . $esphomepm_device_ip;
+
+// Function to extract numeric value from state string
+function extractNumericValue($state) {
+	return floatval(preg_replace('/[^0-9.]/', '', $state));
+}
 
 // Get data from each sensor
 $power = json_decode(file_get_contents($Url . "/sensor/power"), true);
@@ -17,10 +24,10 @@ $voltage = json_decode(file_get_contents($Url . "/sensor/voltage"), true);
 $current = json_decode(file_get_contents($Url . "/sensor/current"), true);
 
 $json = array(
-		'Total' => $energy['state'],
-		'Power' => $power['state'],
-		'Voltage' => $voltage['state'],
-		'Current' => $current['state'],
+		'Total' => $energy ? $energy['value'] : 0,
+		'Power' => $power ? $power['value'] : 0,
+		'Voltage' => $voltage ? $voltage['value'] : 0,
+		'Current' => $current ? $current['value'] : 0,
 		'Costs_Price' => $esphomepm_costs_price,
 		'Costs_Unit' => $esphomepm_costs_unit
 	);
