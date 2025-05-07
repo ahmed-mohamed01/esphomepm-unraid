d6b08155200d04aca64281bf4b05a236b22783aa
abcdw654 <ahmed.mohamed547@gmail.com>
2025-05-07 15:15:32 +0100

    feat: initial implementation of esphome api support in the baackend

--- src/esphomepm/usr/local/emhttp/plugins/esphomepm/status.php
+++ src/esphomepm/usr/local/emhttp/plugins/esphomepm/status.php
@@ -1,47 +1,28 @@
 <?php
-$tasmotapm_cfg = parse_ini_file( "/boot/config/plugins/tasmotapm/tasmotapm.cfg" );
-$tasmotapm_device_ip	= isset($tasmotapm_cfg['DEVICE_IP']) ? $tasmotapm_cfg['DEVICE_IP'] : "";
-$tasmotapm_use_pass	= isset($tasmotapm_cfg['DEVICE_USE_PASS']) ? $tasmotapm_cfg['DEVICE_USE_PASS'] : "false";
-$tasmotapm_device_user	= isset($tasmotapm_cfg['DEVICE_USER']) ? $tasmotapm_cfg['DEVICE_USER'] : "";
-$tasmotapm_device_pass	= isset($tasmotapm_cfg['DEVICE_PASS']) ? $tasmotapm_cfg['DEVICE_PASS'] : "";
-$tasmotapm_costs_price	= isset($tasmotapm_cfg['COSTS_PRICE']) ? $tasmotapm_cfg['COSTS_PRICE'] : "0.0";
-$tasmotapm_costs_unit	= isset($tasmotapm_cfg['COSTS_UNIT']) ? $tasmotapm_cfg['COSTS_UNIT'] : "USD";
+$esphomepm_cfg = parse_ini_file( "/boot/config/plugins/esphomepm/esphomepm.cfg" );
+$esphomepm_device_ip = isset($esphomepm_cfg['DEVICE_IP']) ? $esphomepm_cfg['DEVICE_IP'] : "";
+$esphomepm_costs_price = isset($esphomepm_cfg['COSTS_PRICE']) ? $esphomepm_cfg['COSTS_PRICE'] : "0.27";
+$esphomepm_costs_unit = isset($esphomepm_cfg['COSTS_UNIT']) ? $esphomepm_cfg['COSTS_UNIT'] : "GBP";
 
-
-if ($tasmotapm_device_ip == "") {
-	die("Tasmota Device IP missing!");
+if ($esphomepm_device_ip == "") {
+	die("ESPHome Device IP missing!");
 }
 
-$Url = "http://" . $tasmotapm_device_ip . "/cm?";
+$Url = "http://" . $esphomepm_device_ip;
 
-if ($tasmotapm_use_pass == 1) {
-	if ($tasmotapm_device_user == "") {
-		die("Tasmota username missing!");
-	}
-	if ($tasmotapm_device_pass == "") {
-		die("Tasmota password missing!");
-	}
-
-	$Url = $Url . "user=" . $tasmotapm_device_user . "&password=" . $tasmotapm_device_pass . "&";
-}
-
-$Url = $Url . "cmnd=Status%208";
-
-$datajson = file_get_contents($Url);
-$data = json_decode($datajson, true); 
+$power = json_decode(file_get_contents($Url . "/sensor/power"), true);
+$energy = json_decode(file_get_contents($Url . "/sensor/daily_energy"), true);  // Changed from energy to daily_energy
+$voltage = json_decode(file_get_contents($Url . "/sensor/voltage"), true);
+$current = json_decode(file_get_contents($Url . "/sensor/current"), true);
 
 $json = array(
-		'Total' => $data['StatusSNS']['ENERGY']['Total'],
-		'Today' => $data['StatusSNS']['ENERGY']['Today'],
-		'Yesterday' => $data['StatusSNS']['ENERGY']['Yesterday'],
-		'Voltage' => $data['StatusSNS']['ENERGY']['Voltage'],
-		'Current' => $data['StatusSNS']['ENERGY']['Current'],
-		'ApparentPower' => $data['StatusSNS']['ENERGY']['ApparentPower'],
-		'ReactivePower' => $data['StatusSNS']['ENERGY']['ReactivePower'],
-		'Factor' => $data['StatusSNS']['ENERGY']['Factor'],
-		'Power' => $data['StatusSNS']['ENERGY']['Power'],
-		'Costs_Price' => $tasmotapm_costs_price,
-		'Costs_Unit' => $tasmotapm_costs_unit
+		'Total' => $energy['state'],
+		'Power' => $power['state'],
+		'Voltage' => $voltage['state'],
+		'Current' => $current['state'],
+		'Factor' => $power_factor['state'],
+		'Costs_Price' => $esphomepm_costs_price,
+		'Costs_Unit' => $esphomepm_costs_unit
 	);
 
 header('Content-Type: application/json');
